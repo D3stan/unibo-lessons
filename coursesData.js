c3e86ec836bd102f884f0391a84df679af8b09f9
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-03-13 23:37:01 +0100

    Added 'Magistrali a ciclo unico' and 'English Courses' support, added interactions with Teachers and Courses to open the respective pages on the university website

---
+++ coursesData.js
@@ -0,0 +1,474 @@
+const coursesData = {
+    "CLaBE": {
+        "course_name": "Business and economics",
+        "type": "1cycle"
+    },
+    "EconomiaAziendale": {
+        "course_name": "Economia aziendale",
+        "type": "laurea"
+    },
+    "clei": {
+        "course_name": "Economia dell'impresa",
+        "type": "laurea"
+    },
+    "EconomiaCommercio": {
+        "course_name": "Economia e commercio",
+        "type": "laurea"
+    },
+    "EconomiaMercatiIstituzioni": {
+        "course_name": "Economia, mercati e istituzioni",
+        "type": "laurea"
+    },
+    "EconomicsFinance": {
+        "course_name": "Economics and finance",
+        "type": "1cycle"
+    },
+    "clet": {
+        "course_name": "Economics of tourism and cities",
+        "type": "laurea"
+    },
+    "EconomicsPoliticsSocialSciences": {
+        "course_name": "Economics, politics and social sciences",
+        "type": "1cycle"
+    },
+    "Management": {
+        "course_name": "Management and economics",
+        "type": "1cycle"
+    },
+    "ManagementMarketing": {
+        "course_name": "Management e marketing",
+        "type": "laurea"
+    },
+    "Biotecnologie": {
+        "course_name": "Biotecnologie",
+        "type": "laurea"
+    },
+    "CTF": {
+        "course_name": "Chimica e tecnologia farmaceutiche",
+        "type": "magistralecu"
+    },
+    "Farmacia-Bologna": {
+        "course_name": "Farmacia",
+        "type": "magistralecu"
+    },
+    "Genomics": {
+        "course_name": "Genomics",
+        "type": "1cycle"
+    },
+    "Pharmacy-Rimini": {
+        "course_name": "Pharmacy",
+        "type": "magistralecu"
+    },
+    "ScienzeFarmaceutiche": {
+        "course_name": "Scienze farmaceutiche applicate",
+        "type": "laurea"
+    },
+    "ConsulenteLavoroRelazioniAziendali": {
+        "course_name": "Consulente del lavoro e delle relazioni aziendali",
+        "type": "laurea"
+    },
+    "Giurisprudenza-Bologna": {
+        "course_name": "Giurisprudenza (Campus Bologna)",
+        "type": "magistralecu"
+    },
+    "Giurisprudenza-Ravenna": {
+        "course_name": "Giurisprudenza (Campus Ravenna)",
+        "type": "magistralecu"
+    },
+    "GiuristaImpresa": {
+        "course_name": "Giurista per le imprese e per la pubblica amministrazione",
+        "type": "laurea"
+    },
+    "architettura": {
+        "course_name": "Architettura",
+        "type": "magistralecu"
+    },
+    "ArchitetturaIngegneria": {
+        "course_name": "Architettura-ingegneria",
+        "type": "laurea"
+    },
+    "Building": {
+        "course_name": "Building construction engineering",
+        "type": "1cycle"
+    },
+    "MaterialiCompositiPolimerici": {
+        "course_name": "Compositi polimerici",
+        "type": "laurea"
+    },
+    "DesignProdottoIndustriale": {
+        "course_name": "Design del prodotto industriale",
+        "type": "laurea"
+    },
+    "IngegneriaAerospaziale": {
+        "course_name": "Ingegneria aerospaziale",
+        "type": "laurea"
+    },
+    "IngegneriaBiomedica": {
+        "course_name": "Ingegneria biomedica",
+        "type": "laurea"
+    },
+    "IngegneriaChimicaBiochimica": {
+        "course_name": "Ingegneria chimica e biochimica",
+        "type": "laurea"
+    },
+    "IngegneriaCivile": {
+        "course_name": "Ingegneria civile",
+        "type": "laurea"
+    },
+    "IngegneriaAutomazione": {
+        "course_name": "Ingegneria dell'automazione",
+        "type": "laurea"
+    },
+    "elettrica": {
+        "course_name": "Ingegneria dell'energia elettrica",
+        "type": "laurea"
+    },
+    "IngegneriaScienzeInformatiche": {
+        "course_name": "Ingegneria e scienze informatiche",
+        "type": "laurea"
+    },
+    "IngegneriaElettronicaEnergiaInformazione": {
+        "course_name": "Ingegneria elettronica",
+        "type": "laurea"
+    },
+    "ElettronicaTelecomunicazioni": {
+        "course_name": "Ingegneria elettronica e telecomunicazioni",
+        "type": "laurea"
+    },
+    "IngegneriaEnergetica": {
+        "course_name": "Ingegneria energetica",
+        "type": "laurea"
+    },
+    "IngegneriaGestionale": {
+        "course_name": "Ingegneria gestionale",
+        "type": "laurea"
+    },
+    "IngegneriaInformatica": {
+        "course_name": "Ingegneria informatica",
+        "type": "laurea"
+    },
+    "IngegneriaMeccanicaForli": {
+        "course_name": "Ingegneria meccanica (Campus Forlì)",
+        "type": "laurea"
+    },
+    "IngegneriaMeccanica-Bologna": {
+        "course_name": "Ingegneria meccanica (Campus Bologna)",
+        "type": "laurea"
+    },
+    "IngegneriAmbienTerritorio": {
+        "course_name": "Ingegneria per l'ambiente e il territorio",
+        "type": "laurea"
+    },
+    "meccatronica": {
+        "course_name": "Meccatronica",
+        "type": "laurea"
+    },
+    "TecnicheEdiliziaTerritorio": {
+        "course_name": "Tecniche per l'edilizia e il territorio",
+        "type": "laurea"
+    },
+    "TecnologieSistemiInformatici": {
+        "course_name": "Tecnologie dei sistemi informatici",
+        "type": "laurea"
+    },
+    "LingueLetteratureStraniere": {
+        "course_name": "Lingue e letterature straniere",
+        "type": "laurea"
+    },
+    "LingueTecnologieComunicazioneInterculturale": {
+        "course_name": "Lingue e tecnologie per la comunicazione interculturale",
+        "type": "laurea"
+    },
+    "LingueMercatiCultureAsia": {
+        "course_name": "Lingue, mercati e culture dell'asia e dell'africa mediterranea",
+        "type": "laurea"
+    },
+    "Dietistica": {
+        "course_name": "Dietistica (abilitante alla professione sanitaria di dietista)",
+        "type": "laurea"
+    },
+    "EducazioneProfessionale": {
+        "course_name": "Educazione professionale (abilitante alla professione sanitaria di educatore professionale)",
+        "type": "laurea"
+    },
+    "fisioterapia": {
+        "course_name": "Fisioterapia (abilitante alla professione sanitaria di fisioterapista)",
+        "type": "laurea"
+    },
+    "IgieneDentale": {
+        "course_name": "Igiene dentale (abilitante alla professione sanitaria di igienista dentale)",
+        "type": "laurea"
+    },
+    "Infermieristica": {
+        "course_name": "Infermieristica (abilitante alla professione sanitaria di infermiere) (Campus Bologna)",
+        "type": "laurea"
+    },
+    "Infermieristica-Ravenna": {
+        "course_name": "Infermieristica (abilitante alla professione sanitaria di infermiere) (Campus Ravenna - Faenza)",
+        "type": "laurea"
+    },
+    "Infermieristica-Rimini": {
+        "course_name": "Infermieristica (abilitante alla professione sanitaria di infermiere) (Campus Rimini)",
+        "type": "laurea"
+    },
+    "Logopedia": {
+        "course_name": "Logopedia (abilitante alla professione sanitaria di logopedista)",
+        "type": "laurea"
+    },
+    "MedicinaChirurgia-Ravenna": {
+        "course_name": "Medicina e chirurgia (Campus Ravenna)",
+        "type": "magistralecu"
+    },
+    "MedicinaChirurgia-Forli": {
+        "course_name": "Medicina e chirurgia (Campus Forlì)",
+        "type": "magistralecu"
+    },
+    "MedicinaChirurgia": {
+        "course_name": "Medicina e chirurgia (Campus Bologna)",
+        "type": "magistralecu"
+    },
+    "MedicineAndSurgery": {
+        "course_name": "Medicine and surgery",
+        "type": "singlecycle"
+    },
+    "Odontoiatria": {
+        "course_name": "Odontoiatria e protesi dentaria",
+        "type": "magistralecu"
+    },
+    "ostetricia": {
+        "course_name": "Ostetricia (abilitante alla professione sanitaria di ostetrica/o)",
+        "type": "laurea"
+    },
+    "Podologia": {
+        "course_name": "Podologia (abilitante alla professione sanitaria di podologo)",
+        "type": "laurea"
+    },
+    "PrevenzioneAmbienteLavoro": {
+        "course_name": "Tecniche della prevenzione nell'ambiente e nei luoghi di lavoro (abilitante alla professione sanitaria di tecnico della prevenzione nell'ambiente e nei luoghi di lavoro)",
+        "type": "laurea"
+    },
+    "tlb-bologna": {
+        "course_name": "Tecniche di laboratorio biomedico (abilitante alla professione sanitaria di tecnico di laboratorio biomedico)",
+        "type": "laurea"
+    },
+    "Neurofisiopatologia": {
+        "course_name": "Tecniche di neurofisiopatologia (abilitante alla professione sanitaria di tecnico di neurofisiopatologia)",
+        "type": "laurea"
+    },
+    "TecnicheRadiologiaMedica": {
+        "course_name": "Tecniche di radiologia medica, per immagini e radioterapia (abilitante alla professione sanitaria di tecnico di radiologia medica)",
+        "type": "laurea"
+    },
+    "TecnicheOrtopediche": {
+        "course_name": "Tecniche ortopediche (abilitante alla professione sanitaria di tecnico ortopedico)",
+        "type": "laurea"
+    },
+    "acquacoltura": {
+        "course_name": "Acquacoltura e igiene delle produzioni ittiche",
+        "type": "laurea"
+    },
+    "MedicinaVeterinaria": {
+        "course_name": "Medicina veterinaria",
+        "type": "magistralecu"
+    },
+    "Veterinary": {
+        "course_name": "Veterinary medicine",
+        "type": "singlecycle"
+    },
+    "ScienzeTecnichePsicologiche": {
+        "course_name": "Scienze e tecniche psicologiche",
+        "type": "laurea"
+    },
+    "Astronomia": {
+        "course_name": "Astronomia",
+        "type": "laurea"
+    },
+    "HumanEnvironmentalHealth": {
+        "course_name": "Biology of Human and Environmental Health",
+        "type": "1cycle"
+    },
+    "chimica": {
+        "course_name": "Chimica e chimica dei materiali",
+        "type": "laurea"
+    },
+    "ChimicaAmbiente": {
+        "course_name": "Chimica e tecnologie per l'ambiente e per i materiali (Campus Rimini)",
+        "type": "laurea"
+    },
+    "ChimicaMateriali": {
+        "course_name": "Chimica e tecnologie per l'ambiente e per i materiali (Campus Ravenna - Faenza)",
+        "type": "laurea"
+    },
+    "ChimicaIndustriale": {
+        "course_name": "Chimica industriale",
+        "type": "laurea"
+    },
+    "fisica": {
+        "course_name": "Fisica",
+        "type": "laurea"
+    },
+    "informatica": {
+        "course_name": "Informatica",
+        "type": "laurea"
+    },
+    "InformaticaManagement": {
+        "course_name": "Informatica per il management",
+        "type": "laurea"
+    },
+    "matematica": {
+        "course_name": "Matematica",
+        "type": "laurea"
+    },
+    "MetodologieChimicheProdottiProcessi": {
+        "course_name": "Metodologie chimiche per prodotti e processi",
+        "type": "laurea"
+    },
+    "ScienzaMateriali": {
+        "course_name": "Scienza dei materiali",
+        "type": "laurea"
+    },
+    "ScienzeAmbientali": {
+        "course_name": "Scienze ambientali",
+        "type": "laurea"
+    },
+    "ScienzeBiologiche": {
+        "course_name": "Scienze biologiche",
+        "type": "laurea"
+    },
+    "ScienzeGeologiche": {
+        "course_name": "Scienze geologiche",
+        "type": "laurea"
+    },
+    "ScienzeNaturali": {
+        "course_name": "Scienze naturali",
+        "type": "laurea"
+    },
+    "ScienzeStatistiche": {
+        "course_name": "Scienze statistiche",
+        "type": "laurea"
+    },
+    "EconomiaMarketingAgroIndustriale": {
+        "course_name": "Economia e mercati agro-alimentari",
+        "type": "laurea"
+    },
+    "ProduzioniAnimali": {
+        "course_name": "Produzioni animali",
+        "type": "laurea"
+    },
+    "ScienzeCulturaGastronomia": {
+        "course_name": "Scienze e cultura della gastronomia",
+        "type": "laurea"
+    },
+    "verdepaesaggio": {
+        "course_name": "Scienze e tecnologie per il verde e il paesaggio",
+        "type": "laurea"
+    },
+    "TecnologieAgrarie": {
+        "course_name": "Tecnologie agrarie",
+        "type": "laurea"
+    },
+    "TecnologieAlimentari": {
+        "course_name": "Tecnologie alimentari",
+        "type": "laurea"
+    },
+    "ScienzeTerritorioAmbiente": {
+        "course_name": "Tecnologie per il territorio e l'ambiente agro-forestale",
+        "type": "laurea"
+    },
+    "ViticolturaEnologia": {
+        "course_name": "Viticoltura ed enologia",
+        "type": "laurea"
+    },
+    "EducatoreServiziInfanzia": {
+        "course_name": "Educatore nei servizi per l'infanzia",
+        "type": "laurea"
+    },
+    "EducatoreSocialeCulturaleBologna": {
+        "course_name": "Educatore sociale e culturale (Campus Bologna)",
+        "type": "laurea"
+    },
+    "EducatoreSocialeCulturale-Rimini": {
+        "course_name": "Educatore sociale e culturale (Campus Rimini)",
+        "type": "laurea"
+    },
+    "ScienzeFormazionePrimaria": {
+        "course_name": "Scienze della formazione primaria",
+        "type": "laurea"
+    },
+    "ScienzeMotorieSportive": {
+        "course_name": "Scienze delle attività motorie e sportive (Campus Bologna)",
+        "type": "laurea"
+    },
+    "ScienzeMotorieSportive-Rimini": {
+        "course_name": "Scienze delle attività motorie e sportive (Campus Rimini)",
+        "type": "laurea"
+    },
+    "EuropeanStudies": {
+        "course_name": "European studies",
+        "type": "1cycle"
+    },
+    "InternationalStudies": {
+        "course_name": "International studies",
+        "type": "1cycle"
+    },
+    "ScienzeInternazionaliDiplomatiche": {
+        "course_name": "Scienze internazionali e diplomatiche",
+        "type": "laurea"
+    },
+    "ScienzePoliticheSocialiInternazionali": {
+        "course_name": "Scienze politiche, sociali e internazionali",
+        "type": "laurea"
+    },
+    "SviluppoCooperazioneInternazionale": {
+        "course_name": "Sviluppo e cooperazione internazionale",
+        "type": "laurea"
+    },
+    "StatisticaFinanzaAssicurazioni": {
+        "course_name": "Statistica, finanza e assicurazioni",
+        "type": "laurea"
+    },
+    "ServizioSociale": {
+        "course_name": "Servizio sociale",
+        "type": "laurea"
+    },
+    "SociologiaForli": {
+        "course_name": "Sociologia",
+        "type": "laurea"
+    },
+    "ScienzeAntropologiche": {
+        "course_name": "Antropologia, religioni, civiltà orientali",
+        "type": "laurea"
+    },
+    "BeniCulturali": {
+        "course_name": "Beni culturali",
+        "type": "laurea"
+    },
+    "ConservazioneRestauroBeniculturali": {
+        "course_name": "Conservazione e restauro dei beni culturali (abilitante ai sensi del d. lgs n. 42/2004)",
+        "type": "magistralecu"
+    },
+    "CulturePraticheModa": {
+        "course_name": "Culture e pratiche della moda",
+        "type": "laurea"
+    },
+    "DAMS": {
+        "course_name": "Dams - discipline delle arti, della musica e dello spettacolo",
+        "type": "laurea"
+    },
+    "Filosofia": {
+        "course_name": "Filosofia",
+        "type": "laurea"
+    },
+    "lettere": {
+        "course_name": "Lettere",
+        "type": "laurea"
+    },
+    "ScienzeComunicazione": {
+        "course_name": "Scienze della comunicazione",
+        "type": "laurea"
+    },
+    "storia": {
+        "course_name": "Storia",
+        "type": "laurea"
+    }
+};
